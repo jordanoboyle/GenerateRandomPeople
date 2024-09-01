50a5e5cc1f698bcc29170c2cc20ba8b7ec4177df
jordanoboyle <jmcmcode@gmail.com>
2024-08-31 22:36:06 -0600

    DOM elements needed defined

---
+++ script.js
@@ -0,0 +1,13 @@
+//DOM Elements
+const main = document.getElementById('main');
+const addUserButton = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double-money');
+const showMillionairesBtn = document.getElementById('show-millaires');
+const sortBtn = document.getElementById('sort-people');
+const calculateWeatlthBtn = document.getElementById('calculate-wealth');
+
+
+let data = [];
+
+//Fetch from generate random user from API
+// "https://randomuser.me/"
